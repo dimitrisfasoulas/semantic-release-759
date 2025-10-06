c748387fa1bc3681afe3f50663e50c0b6ac7a5fa
Dimitrios Fasoulas <dimitrios.fasoulas@axiomatics.com>
2025-10-06 22:18:03 +0200

    Added semantic release config

---
+++ release.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: ['master', 'next', 'test_release'],
+  repositoryUrl: 'git@github.com:dimitrisfasoulas/semantic-release-759.git',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits'
+      }
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): [XX-000] ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ]
+};
